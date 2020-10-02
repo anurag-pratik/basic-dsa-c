67bff1d704fb76caf60d31ab4eac88055c562c00
rachna-shree <72251922+rachna-shree@users.noreply.github.com>
2020-10-02 15:04:18 +0530

    Update POLY_ARRAY.C

--- POLY_ARRAY.C
+++ POLY_ARRAY.C
@@ -1,6 +1,6 @@
 #include<stdio.h>
 
-int d1,d2,i,j,l;
+int da1,da2,i,j,l;
 
 int max(int a, int b)
 {
@@ -20,10 +20,10 @@ void add(int arr1[], int arr2[])
 
     printf("\nPolynomial 1 + Polynomial 2:\n");
   
-    for(i=0;i<=d1;i++)
+    for(i=0;i<=da1;i++)
      arr3[i]+=arr1[i];
 
-    for(i=0;i<=d2;i++)
+    for(i=0;i<=da2;i++)
      arr3[i]+=arr2[i];
     
     for(i=0;i<=l;i++)
@@ -38,15 +38,15 @@ void add(int arr1[], int arr2[])
 
 void mull(int arr1[],int arr2[])
 {
-    int arrm[d1+d2+1];
+    int arrm[da1+da2+1];
 
-    for(i=0;i<=d1+d2;i++)
+    for(i=0;i<=da1+da2;i++)
      arrm[i]=0;
 
 
-    for(i=0;i<=d1;i++)
+    for(i=0;i<=da1;i++)
     {
-        for(j=0;j<=d2;j++)
+        for(j=0;j<=da2;j++)
         {
          arrm[i+j]+=(arr1[i]*arr2[j]);
         }
@@ -54,12 +54,12 @@ void mull(int arr1[],int arr2[])
 
     printf("\n\nPolynomial 1 x Polynomial 2:\n");
 
-    for(i=0;i<=d1+d2;i++)
+    for(i=0;i<=da1+da2;i++)
     {
      printf("%d ",arrm[i]);
      if(i!=0)
      printf("x^%d ",i);
-     if(i<d1+d2)
+     if(i<da1+da2)
       printf("+ ");
     }
 }
@@ -68,24 +68,24 @@ void mull(int arr1[],int arr2[])
 int main()
 {
     printf("Enter the highest degree of the polynomial 1: ");
-    scanf("%d",&d1);
-    int arr1[d1+1];
-    for(i=0;i<=d1;i++)
+    scanf("%d",&da1);
+    int arr1[da1+1];
+    for(i=0;i<=da1;i++)
     {
         printf("Enter coefficient for power %d: ",i);
         scanf("%d",&arr1[i]);
     }
 
     printf("Enter the highest degree of the polynomial 2: ");
-    scanf("%d",&d2);
-    int arr2[d2+1];
-    for(i=0;i<=d2;i++)
+    scanf("%d",&da2);
+    int arr2[da2+1];
+    for(i=0;i<=da2;i++)
     {
         printf("Enter coefficient for power %d: ",i);
         scanf("%d",&arr2[i]);
     }
 
-    l=max(d1,d2);
+    l=max(da1,da2);
     add(arr1,arr2);
     mull(arr1,arr2);
     return 0;
